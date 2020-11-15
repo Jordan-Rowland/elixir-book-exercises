e29784dfd389e30040b9d6a8ac08d6a457254cd7
Jordan Rowland <36084892+Jordan-Rowland@users.noreply.github.com>
2020-11-15 09:37:18 -0800

    added sockets, mismatch in versions

---
+++ hangman_game/socket_gallows/lib/socket_gallows_web/endpoint.ex
@@ -0,0 +1,48 @@
+defmodule SocketGallowsWeb.Endpoint do
+  use Phoenix.Endpoint, otp_app: :socket_gallows
+
+  @session_options [
+    store: :cookie,
+    key: "_socket_gallows_key",
+    signing_salt: "F9vPOPAM"
+  ]
+
+  socket("/socket", SocketGallowsWeb.UserSocket,
+    websocket: true,
+    longpoll: false
+  )
+
+  socket("/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]])
+
+  plug(Plug.Static,
+    at: "/",
+    from: :socket_gallows,
+    gzip: false,
+    only: ~w(css fonts images js favicon.ico robots.txt)
+  )
+
+  if code_reloading? do
+    socket("/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket)
+    plug(Phoenix.LiveReloader)
+    plug(Phoenix.CodeReloader)
+  end
+
+  plug(Phoenix.LiveDashboard.RequestLogger,
+    param_key: "request_logger",
+    cookie_key: "request_logger"
+  )
+
+  plug(Plug.RequestId)
+  plug(Plug.Telemetry, event_prefix: [:phoenix, :endpoint])
+
+  plug(Plug.Parsers,
+    parsers: [:urlencoded, :multipart, :json],
+    pass: ["*/*"],
+    json_decoder: Phoenix.json_library()
+  )
+
+  plug(Plug.MethodOverride)
+  plug(Plug.Head)
+  plug(Plug.Session, @session_options)
+  plug(SocketGallowsWeb.Router)
+end
